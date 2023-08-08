52f9b00bbd8176c5dcbbf14c7a46334f9fcbe03d
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2023-08-08 15:17:06 -0300

    Initial commit

---
+++ Assets/Scripts/ColectivoScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColectivoScript : MonoBehaviour
+{
+    public float capacidadTanque;
+    public float cargaActual;
+    public GameObject WarningSign;
+}
---
+++ Assets/Scripts/ColectivosManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColectivosManager : MonoBehaviour
+{
+
+    [SerializeField] ColectivoScript[] colectivos;
+    [SerializeField] float maxCapacidad = 60;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    private void CargarNaftaRandomATodosLosColectivos()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ActivarTodosLosColectivos();
+            AsignarCapacidadRandomALosColectivos();
+            CargarNaftaRandomATodosLosColectivos();
+            DesactivarTodosLosColectivos();
+            ActivarColectivosConMenosDeMedioTanque();
+            ActivarSignoDeAdvertenciaEnColectivosConMenosDe10DeCombustible();
+            MostrarPromedioDeCombustible();
+            MostrarCombustibleFaltanteParaLlenarTodosLosTanques();
+        }
+    }
+
+    private void AsignarCapacidadRandomALosColectivos()
+    {
+
+    }
+
+
+    private void ActivarColectivosConMenosDeMedioTanque()
+    {
+
+    }
+
+    private void DesactivarTodosLosColectivos()
+    {
+
+    }
+
+
+    private void ActivarTodosLosColectivos()
+    {
+
+    }
+
+    private void ActivarSignoDeAdvertenciaEnColectivosConMenosDe10DeCombustible()
+    {
+
+    }
+
+    private void MostrarPromedioDeCombustible()
+    {
+
+    }
+
+    private void MostrarCombustibleFaltanteParaLlenarTodosLosTanques()
+    {
+
+    }
+}
